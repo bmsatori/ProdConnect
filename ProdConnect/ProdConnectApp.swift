7a45f9a9efadcabc251a183df86242f0eb1c1e75
Benjy Satorius <benjysatorius@gmail.com>
2025-10-24 12:41:36 -0500

    Initial Commit

---
+++ ProdConnect/ProdConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdConnectApp.swift
+//  ProdConnect
+//
+//  Created by Benjy Satorius on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ProdConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
